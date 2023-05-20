fdc8336b1af595f3781ebbdb632258076b8ca5fd
Asadbek <asadbekn443@gmail.com>
2023-05-20 12:30:48 -0400

    Scaling Effect

--- ExpandingViewAnimation/ContentView.swift
+++ ExpandingViewAnimation/ContentView.swift
@@ -8,14 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isAnimating: Bool = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            ZStack {
+                ExpandingView(expand: $isAnimating, direction: .bottom, symbolName: "note.text")
+                ExpandingView(expand: $isAnimating, direction: .left, symbolName: "doc")
+                ExpandingView(expand: $isAnimating, direction: .top, symbolName: "photo")
+                ExpandingView(expand: $isAnimating, direction: .right, symbolName: "mic.fill")
+                
+                Image(systemName: "plus").font(.system(size: 40, weight: isAnimating ? .regular : .semibold, design: .rounded))
+                    .foregroundColor(isAnimating ? Color.white : Color.black)
+                    .rotationEffect(isAnimating ? .degrees(45) : .degrees(0))
+                    .scaleEffect(isAnimating ? 3 : 1)
+                    .opacity(isAnimating ? 0.5 : 1)
+                    .animation(Animation.spring(response: 0.35, dampingFraction: 0.85, blendDuration: 1), value: isAnimating)
+                    .onTapGesture {
+                        isAnimating .toggle()
+                    }
+            }
         }
-        .padding()
+           
     }
 }
 
