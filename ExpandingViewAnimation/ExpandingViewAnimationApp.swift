ad0b4cf7024f5568cc5f50c8d4ddd3058bf4e0a5
Asadbek <asadbekn443@gmail.com>
2023-05-20 12:28:39 -0400

    Initial Commit

---
+++ ExpandingViewAnimation/ExpandingViewAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpandingViewAnimationApp.swift
+//  ExpandingViewAnimation
+//
+//  Created by Asadbek Nematov on 5/20/23.
+//
+
+import SwiftUI
+
+@main
+struct ExpandingViewAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
